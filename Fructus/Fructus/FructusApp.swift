c2555e3d29e60599cca024f7a95fdf186b5a98cb
Carlos Cáceres González <carlos.caceres.86@gmail.com>
2021-05-07 23:26:33 +0200

    First Commit
    First commit. Create basic project. Add resources

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Carlos Cáceres González on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
